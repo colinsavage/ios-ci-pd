0bc54cc8f2f5014d7e251ee86c47f145022224aa
Colin Savage <colin@savage.org.za>
2020-03-26 13:46:55 +1000

    New files

---
+++ SeeEyePeeDee/SeeEyePeeDee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeEyePeeDee
+//
+//  Created by Colin Savage on 26/3/20.
+//  Copyright © 2020 Colin Savage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SeeEyePeeDee/SeeEyePeeDeeTests/SeeEyePeeDeeTests.swift
@@ -0,0 +1,34 @@
+//
+//  SeeEyePeeDeeTests.swift
+//  SeeEyePeeDeeTests
+//
+//  Created by Colin Savage on 26/3/20.
+//  Copyright © 2020 Colin Savage. All rights reserved.
+//
+
+import XCTest
+@testable import SeeEyePeeDee
+
+class SeeEyePeeDeeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
